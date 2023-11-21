6d2f7711654fadf8aeb963e035bde57d3db3a497
Yuri Garcia <143082990+Yur1Garc1a@users.noreply.github.com>
2023-11-21 09:53:26 -0300

    Update Exercicio5.java Comentários e Operador Ternário

--- P.A-main/tpa/WorkSpace-TPA/1BIM/aula9(Switch)/Exercicio5.java
+++ P.A-main/tpa/WorkSpace-TPA/1BIM/aula9(Switch)/Exercicio5.java
@@ -1,73 +1,61 @@
 import java.util.Scanner;
-public class Exercicio5 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
-		int v1,v2;
-		System.out.println("Digite o Mes em que nasceu");
-		v1 = in.nextInt();
-		switch(v1) {
-		case 1:
-			System.out.println("Digite o dia");
-			v2 = in.nextInt();
-			if (v2 <= 20) {System.out.println("Capricornio");}
-			else {System.out.println("Aquario");}break;
-			
-		case 2:	System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 < 19) {System.out.println("Aquario");}
-		else {System.out.println("Peixes");}break;
-		
-		case 3:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 20) {System.out.println("Peixes");}
-		else {System.out.println("Aries");}break;
-			
-		case 4:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 20) {System.out.println("Aries");}
-		else {System.out.println("Touro");}break;
-			
-		case 5:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 20) {System.out.println("Touro");}
-		else {System.out.println("Gemeos");}break;
-			
-		case 6:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 20) {System.out.println("Gemeos");}
-		else {System.out.println("Cancer");}break;
-			
-		case 7:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 22) {System.out.println("Cancer");}
-		else {System.out.println("Leao");}break;
-			
-		case 8:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 22) {System.out.println("Leao");}
-		else {System.out.println("virgem");}break;
-			
-		case 9:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 22) {System.out.println("Virgem");}
-		else {System.out.println("Libra");}break;
-		
-		case 10:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 22) {System.out.println("Libra");}
-		else {System.out.println("escorpiao");}break;
-			
-		case 11:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 21) {System.out.println("escorpiao");}
-		else {System.out.println("Sagitario");}break;
-			
-		case 12:System.out.println("Digite o dia");
-		v2 = in.nextInt();
-		if (v2 <= 21) {System.out.println("Sagitario");}
-		else {System.out.println("Capricornio");}break;
-		default:System.out.println("mes invalido");
-			}
-		in.close();
-	}
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int mes, dia;
+
+        System.out.println("Digite o mês em que nasceu");
+        mes = in.nextInt();
+
+        if (mes < 1 || mes > 12) {
+            System.out.println("Mês inválido");
+        } else {
+            System.out.println("Digite o dia");
+            dia = in.nextInt();
+
+		// Usei o operador condicional ternário//
+		//? verifica se a condição é verdeira, em seguida a damos a primeira resposta caso seja
+		// depois vem : e do outro lado a resposta caso não seja
+            switch (mes) {
+                case 1:
+                    System.out.println(dia <= 20 ? "Capricórnio" : "Aquário");
+                    break;
+                case 2:
+                    System.out.println(dia < 19 ? "Aquário" : "Peixes");
+                    break;
+                case 3:
+                    System.out.println(dia <= 20 ? "Peixes" : "Áries");
+                    break;
+                case 4:
+                    System.out.println(dia <= 20 ? "Áries" : "Touro");
+                    break;
+                case 5:
+                    System.out.println(dia <= 20 ? "Touro" : "Gêmeos");
+                    break;
+                case 6:
+                    System.out.println(dia <= 20 ? "Gêmeos" : "Câncer");
+                    break;
+                case 7:
+                    System.out.println(dia <= 22 ? "Câncer" : "Leão");
+                    break;
+                case 8:
+                    System.out.println(dia <= 22 ? "Leão" : "Virgem");
+                    break;
+                case 9:
+                    System.out.println(dia <= 22 ? "Virgem" : "Libra");
+                    break;
+                case 10:
+                    System.out.println(dia <= 22 ? "Libra" : "Escorpião");
+                    break;
+                case 11:
+                    System.out.println(dia <= 21 ? "Escorpião" : "Sagitário");
+                    break;
+                case 12:
+                    System.out.println(dia <= 21 ? "Sagitário" : "Capricórnio");
+                    break;
+            }
+        }
+
+        in.close();
+    }
+}           
